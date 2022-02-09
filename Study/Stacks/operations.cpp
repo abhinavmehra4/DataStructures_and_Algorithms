d25497bcaac07bd3e05afbef0670cdbcd2207944
abhinavmehra4 <mehraabhinav40@gmail.com>
2022-02-09 21:17:45 +0530

    Changes to the operation functions

--- Study/Stacks/operations.cpp
+++ Study/Stacks/operations.cpp
@@ -9,66 +9,57 @@ public:
     int a[MAX]; //Maximum size of the stack
     Stack(){ top = -1; }//A constructor is created so that every time a stack is created, it is
     //initialised to -1 
-    bool push(int x);
-    int pop();
-    int peek();
-    bool isEmpty();
-
-
-};
-
-bool Stack :: push(int x)
-{
-    //before pushing one must check if the stack is full
-    if(top>=MAX-1)
-    {
-        cout<<"Stack Overflow";
-        return false;
-    }
-    else
-    {
-        a[++top]=x; //Since value of top is same as index of the array
-        //in the above step, value was stored in the stack as well as an increment in the value of top took place.
-        cout<< x <<" was pushed into the stack\n";
-        return true;
-    }
+    bool push(int x)
+        {
+                //before pushing one must check if the stack is full
+                if(top>=MAX-1)
+                {
+                    cout<<"Stack Overflow";
+                    return false;
+                }
+                else
+                {
+                    a[++top]=x; //Since value of top is same as index of the array
+                    //in the above step, value was stored in the stack as well as an increment in the value of top took place.
+                    cout<< x <<" was pushed into the stack\n";
+                    return true;
+                }
+            
     
-}
-
-int Stack ::pop()
-//this function returns the value that is to be popped
-{
-    //before popping we must check if the stack is already empty or not
-    if(top<0)
-    {
-        cout<<"Stack underflow\n";
-        return 0;
-    }
-    else
-    {
-        int x = a[top--]; //simultaneously value of top is decreased.
-        return x;
-    }
-}
-
-int Stack::peek()
-{   
-    //Again, check the nullity of the stack
-    if (top < 0) {
-        cout << "Stack is Empty";
-        return 0;
-    }
-    //assign the values of the stack to new temporary variables and return that value.
-    else {
-        int x = a[top];//value of top is not changed here.
-        return x;
-    }
-}
-
-bool Stack::isEmpty()
-{
-    return (top < 0); //top must be -1 to show that the stack is empty
-}
+        }
+    int pop()
+        //this function returns the value that is to be popped
+            {
+                //before popping we must check if the stack is already empty or not
+                if(top<0)
+                {
+                    cout<<"Stack underflow\n";
+                    return 0;
+                }
+                else
+                {
+                    int x = a[top--]; //simultaneously value of top is decreased.
+                    return x;
+                }
+            }
+    int peek()
+        {   
+            //Again, check the nullity of the stack
+            if (top < 0) {
+                cout << "Stack is Empty";
+                return 0;
+            }
+            //assign the values of the stack to new temporary variables and return that value.
+            else {
+                int x = a[top];//value of top is not changed here.
+                return x;
+            }
+        }
+    bool isEmpty()
+        {
+            return (top < 0); //top must be -1 to show that the stack is empty
+        }
+};
 
 void printStack(Stack s)
 {
