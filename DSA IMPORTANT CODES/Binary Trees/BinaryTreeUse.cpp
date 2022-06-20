d25bac31d54521bd3e19ae27a137233f2e9acf3e
abhinavmehra4 <mehraabhinav40@gmail.com>
2022-06-20 12:14:58 +0530

    Started Binary Trees

---
+++ DSA IMPORTANT CODES/Binary Trees/BinaryTreeUse.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include "BinaryTreeNode.h"
+using namespace std;
+
+void printTree(BinaryTreeNode<int>* root)
+{
+    /*
+    cout<<root->data;
+    printTree(root->left);
+    printTree(root->right);
+
+Bu this will not be good because, evetually this function will try to print a root->data for a NULL when it comes to a leaf node
+    */
+   /*
+   This is for printing without specifying subtrees
+            if(root==NULL)
+                    return;
+                cout<<root->data<<endl;
+                printTree(root->left);
+                printTree(root->right); 
+   */
+    if(root==NULL)
+        return;
+    cout<<root->data<<":";
+    if(root->left)
+    {
+        cout<<"L"<<root->left->data;
+    }
+    if(root->right)
+    {
+        cout<<"R:"<<root->right->data;
+    }
+    cout<<endl; //for moving on to the next subtree
+    printTree(root->left);
+    printTree(root->right);
+}
+
+int main()
+{
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(1);
+    BinaryTreeNode<int>* node1 = new BinaryTreeNode<int>(2);
+    BinaryTreeNode<int>* node2 = new BinaryTreeNode<int>(3);
+    root->left=node1;
+    root->right = node2;
+    printTree(root);
+    delete root;
+    return 0;
+}
---
+++ DSA IMPORTANT CODES/Binary Trees/takeLevelWiseInput.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include "BinaryTreeNode.h"
+using namespace std;
+void printTree(BinaryTreeNode<int>* root)
+{   if(root==NULL)
+        return;
+    cout<<root->data<<":";
+    if(root->left)
+    {
+        cout<<"L"<<root->left->data;
+    }
+    if(root->right)
+    {
+        cout<<"R"<<root->right->data;
+    }
+    cout<<endl;
+    printTree(root->left);
+    printTree(root->right);
+}
+
+BinaryTreeNode<int>* takeInput()
+{
+        /*
+                The flow works in such a way that whener we call this function, left root is always taken 
+                as input first, then the right one. But to specify if a particular subtrees left or right node is to be left
+                NULL, we will ask the user to enter -1
+        */
+        int rootData;
+        cout<<"Enter data "<<endl;
+        cin>>rootData;
+        if(rootData==-1)
+        {return NULL;}
+        //But now that we have reached here, means that the user wants a node in the left or right. So we create a node
+        BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+        BinaryTreeNode<int>* leftChild = takeInput();
+        BinaryTreeNode<int>* rightChild = takeInput();
+        root->left = leftChild;
+        root->right = rightChild;
+        return root;
+}
+
+BinaryTreeNode<int>* takeInputLevelWise()
+{
+    
+}
+
+int main()
+{
+    BinaryTreeNode<int>* root = takeInput();
+    printTree(root);
+}
