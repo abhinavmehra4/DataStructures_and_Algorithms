7871277f42a13b174607ecbd6540f35e6cc39056
abhinavmehra4 <mehraabhinav40@gmail.com>
2022-07-16 15:16:48 +0530

    additional work

---
+++ DSA IMPORTANT CODES/DYNAMIC PROGRAMMING/knapsackProblem.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int knapsack(int *weights, int *values, int n, int maxWeight)
+{
+    //base case- when array is empty and in such a case return 0
+
+    
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int *weights = new int[n];
+	int *values = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> weights[i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> values[i];
+	}
+
+	int maxWeight;
+	cin >> maxWeight;
+
+	cout << knapsack(weights, values, n, maxWeight) << endl;
+
+	delete[] weights;
+	delete[] values;
+}
